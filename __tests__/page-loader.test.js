c1625497fc6f4d13df3f77ef477f9548db8f5fed
BudenchukM <budenchuk.marina@gmail.com>
2025-07-04 15:48:19 +0300

    lint6

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -15,6 +15,7 @@ describe('page-loader (promise version)', () => {
   const pageUrl = `${baseUrl}/courses`
 
   let originalHtml
+  let expectedHtml
 
   beforeAll(async () => {
     const fixturesPath = path.join(__dirname, '../__fixtures__/hexlet-page')
